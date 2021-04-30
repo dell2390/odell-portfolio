bcdf58e4c258ea5e70cd35216b09857e4981ed54
Isaiah Smith <jordankid93@gmail.com>
2021-04-30 11:59:48 -0700

    chore: setup storyshots
    Jest config moduleDirectories needed "<rootDir>" entry

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,6 @@
 module.exports = {
+    verbose: true,
     setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
     testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
+    moduleDirectories: ["node_modules", "<rootDir>"],
 }
