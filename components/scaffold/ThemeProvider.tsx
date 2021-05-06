207e979f105fb309e9dafa82bb25c13494bec2f2
Isaiah Smith <jordankid93@gmail.com>
2021-05-06 10:01:04 -0700

    chore: add photos marquee
    - also fixes SSG styling issues

--- components/scaffold/ThemeProvider.tsx
+++ components/scaffold/ThemeProvider.tsx
@@ -1,39 +1,9 @@
 import { FC } from "react"
-import {
-    ThemeProvider as MuiThemeProvider,
-    createMuiTheme,
-    responsiveFontSizes,
-} from "@material-ui/core"
+import { ThemeProvider as MuiThemeProvider } from "@material-ui/core"
+
+import theme from "lib/theme"
 
 const ThemeProvider: FC = ({ children }) => {
-    const theme = responsiveFontSizes(
-        createMuiTheme({
-            typography: {
-                fontFamily: ["Lato", "sans-serif"].join(","),
-            },
-            palette: {
-                primary: {
-                    main: "#FFA726",
-                },
-            },
-            overrides: {
-                MuiButton: {
-                    label: {
-                        textTransform: "none",
-                        fontWeight: "bold",
-                        letterSpacing: "1px",
-                    },
-                },
-
-                MuiLink: {
-                    root: {
-                        color: "inherit",
-                    },
-                },
-            },
-        })
-    )
-
     return <MuiThemeProvider theme={theme}>{children}</MuiThemeProvider>
 }
 
