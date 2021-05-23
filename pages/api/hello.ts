253fa3889a88271b7947bd9e8e356cb0bd798869
Isaiah Smith <jordankid93@gmail.com>
2021-05-23 16:26:52 -0700

    chore: remove unused api route

--- pages/api/hello.ts
+++
@@ -1,8 +0,0 @@
-import { VercelRequest, VercelResponse } from "@vercel/node"
-
-const hello = (request: VercelRequest, response: VercelResponse) => {
-    const { name = "World" } = request.query
-    response.status(200).send(`Hello ${name}!`)
-}
-
-export default hello
